4760443e92b429cf0c20e59694969b69b93df6c7
jakoobishak <jakoob_ishak@hotmail.com>
2017-09-12 19:42:20 +0200

    3 shapes done

---
+++ shape.h
@@ -0,0 +1,17 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include <QWidget>
+
+class Shape : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Shape(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // SHAPE_H
